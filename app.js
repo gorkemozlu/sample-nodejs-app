d6fab267f2fb7a0e54516433c71cf0c82345cf41
Gorkem Ozlu <spammailatmayin@gmail.com>
2022-01-26 15:03:43 +0300

    update app with vars

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var os = require("os");
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  //res.send('Hello World! ' + process.env.DB_HOST)
+  function format(seconds){
+    function pad(s){
+      return (s < 10 ? '0' : '') + s;
+    }
+    var hours = Math.floor(seconds / (60*60));
+    var minutes = Math.floor(seconds % (60*60) / 60);
+    var seconds = Math.floor(seconds % 60);
+  
+    return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds);
+  }
+  var hostname = os.hostname();
+
+  var uptime = process.uptime();
+  //console.log(format(uptime));
+  //console.log(hostname)
+  res.send(`<p>Hello  ${process.env.var1} !</p><p>Node Name: ${process.env.MY_NODE_NAME}</p><p>Pod Name: ${process.env.MY_POD_NAME}</p><p>Namespace: ${process.env.MY_POD_NAMESPACE}</p><p>Service Account: ${process.env.MY_POD_SERVICE_ACCOUNT}</p><p>Uptime: ${uptime}</p>`)
+  
 })
 
 app.listen(port, () => {
